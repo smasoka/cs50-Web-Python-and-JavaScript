e4da081d865ac16890f0adbfc6dd044fd0f49614
Sakhile Masoka <sakhile.harvey@gmail.com>
2020-09-29 14:05:50 +0200

    complete search

--- django/wiki/encyclopedia/urls.py
+++ django/wiki/encyclopedia/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("wiki/<str:title>", views.page, name="page"),
+    path("search", views.search, name="search"),
 ]
--- django/wiki/encyclopedia/views.py
+++ django/wiki/encyclopedia/views.py
@@ -13,26 +13,24 @@ def index(request):
 
 def page(request, title):
     
-    if title in util.list_entries():
-        fweb = markdown2.markdown(util.get_entry(title))
+    fweb = markdown2.markdown(util.get_entry(title))
 
-        return render(request, "encyclopedia/page.html", {
-            "title": title,
-            "fweb": fweb
-        })
+    return render(request, "encyclopedia/page.html", {
+        "title": title is not None,
+        "fweb": fweb if fweb is not None else "Not Found"
+    })
+
+def search(request):
+    query = request.GET.get('q')
+    
+    if util.get_entry(query):
+        return HttpResponseRedirect(reverse("page", args=(query,)))
     else:
-        return HttpResponse("Entry not found!")
-
-def search_results(request):
-    if request.method == "POST":
-        query = request.POST.get('q')
-        if query in util.list_entries():
-            return HttpResponseRedirect(reverse("page", query))
-        else:
-            query_list = []
-            for entry in util.list_entries:
-                if query in entry:
-                    query_list.append(entry)
-            return render(request, "encyclopedia/search_results", {
-                "query_list": query_list
-            })
+        matched_list = []
+        for entry in util.list_entries():
+            if query in entry:
+                matched_list.append(entry)
+        
+        return render(request, "encyclopedia/search.html", {
+            "matched_list": matched_list
+        })
